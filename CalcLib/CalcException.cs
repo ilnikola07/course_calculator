54be8c402b53ec58e05e4cf20f3b3129fbc81a39
ilnikola07 <nikilushin07@gmail.com>
2026-04-23 22:38:45 +0300

    добавлено содержимое в класс CalcExeption

--- CalcLib/CalcException.cs
+++ CalcLib/CalcException.cs
@@ -6,11 +6,65 @@ using System.Threading.Tasks;
 
 namespace CalcLib
 {
-    internal class CalcException : Exception
+    public enum CalcErrorCode
     {
-        public CalcException(string message) : base(message) 
-        {
+        Unknown,
+        DivisionByZero,
+        InvalidVariable,
+        InvalidFunction,
+        InvalidOperator,
+        SyntaxError,
+        MathDomainError,    // логарифм от отрицательного, корень из отрицательного
+        StackUnderflow,     // не хватает операндов
+        ResultNaN,
+        ResultInfinity
+    }
+    public class CalcException : Exception
+    {
+        public CalcErrorCode ErrorCode { get; }
+        public string Token { get; }          // проблемный токен (если есть)
+        public int Position { get; }          // позиция в выражении (если известно)
+        public string Suggestion { get; }     // подсказка по исправлению
 
+        public CalcException(string message, CalcErrorCode code = CalcErrorCode.Unknown)
+            : base(message)
+        {
+            ErrorCode = code;
         }
+
+        public CalcException(string message, CalcErrorCode code, string token)
+            : this(message, code)
+        {
+            Token = token;
+        }
+
+        public CalcException(string message, CalcErrorCode code, int position)
+            : this(message, code)
+        {
+            Position = position;
+        }
+
+
+        public static CalcException DivisionByZero() =>
+            new CalcException("Деление на ноль!", CalcErrorCode.DivisionByZero);
+
+        public static CalcException InvalidVariable(string name) =>
+            new CalcException($"Неизвестная переменная: '{name}'",
+                             CalcErrorCode.InvalidVariable, name);
+
+        public static CalcException InvalidFunction(string name) =>
+            new CalcException($"Неизвестная функция: '{name}'",
+                             CalcErrorCode.InvalidFunction, name);
+
+        public static CalcException MathDomainError(string operation, double value) =>
+            new CalcException($"Недопустимое значение для {operation}: {value}",
+                             CalcErrorCode.MathDomainError);
+
+        public static CalcException SyntaxError(string details) =>
+            new CalcException($"Синтаксическая ошибка: {details}",
+                             CalcErrorCode.SyntaxError);
     }
 }
+
+
+    
